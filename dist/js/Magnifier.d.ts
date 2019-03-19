f379dccd4bacae211658134870afc65afee18e0e
eliataylor <eli@taylormadetraffic.com>
2019-03-19 12:00:45 -0700

    add dist for easier development of fork

---
+++ dist/js/Magnifier.d.ts
@@ -0,0 +1,70 @@
+import React, { PureComponent } from "react";
+import "./style.scss";
+declare type mgShape = "circle" | "square";
+interface Props {
+	src: string;
+	triggerEl: object;
+	width?: string | number;
+	height?: string | number;
+	className?: string;
+	zoomImgSrc?: string;
+	zoomFactor?: number;
+	mgWidth?: number;
+	mgHeight?: number;
+	mgBorderWidth?: number;
+	mgShape?: mgShape;
+	mgShowOverflow?: boolean;
+	mgMouseOffsetX?: number;
+	mgMouseOffsetY?: number;
+	mgTouchOffsetX?: number;
+	mgTouchOffsetY?: number;
+}
+interface State {
+	showZoom: boolean;
+	mgOffsetX: number;
+	mgOffsetY: number;
+	relX: number;
+	relY: number;
+}
+export default class Magnifier extends PureComponent<Props, State> {
+	state: {
+		showZoom: boolean;
+		mgOffsetX: number;
+		mgOffsetY: number;
+		relX: number;
+		relY: number;
+	};
+	img: HTMLElement;
+	triggerEl: HTMLElement;
+	imgBounds: DOMRect | ClientRect;
+	static defaultProps: {
+		width: string;
+		height: string;
+		className: string;
+		triggerEl: boolean;
+		zoomFactor: number;
+		mgWidth: number;
+		mgHeight: number;
+		mgBorderWidth: number;
+		mgShape: string;
+		mgShowOverflow: boolean;
+		mgMouseOffsetX: number;
+		mgMouseOffsetY: number;
+		mgTouchOffsetX: number;
+		mgTouchOffsetY: number;
+	};
+	constructor(props: Props);
+	componentDidMount(): void;
+	componentWillUnmount(): void;
+	onMouseEnter(): void;
+	onMouseMove(e: MouseEvent): void;
+	onMouseOut(): void;
+	onTouchStart(e: TouchEvent): void;
+	onTouchMove(e: TouchEvent): void;
+	onTouchEnd(): void;
+	calcImgBounds(): void;
+	calcImgBoundsDebounced: () => void;
+	render(): React.ReactElement;
+}
+export {};
+//# sourceMappingURL=Magnifier.d.ts.map
---
+++ dist/js/Magnifier.es.js
@@ -0,0 +1,348 @@
+import React, { PureComponent } from "react";
+import debounce from "lodash.debounce";
+import throttle from "lodash.throttle";
+
+/*! *****************************************************************************
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this file except in compliance with the License. You may obtain a copy of the
+License at http://www.apache.org/licenses/LICENSE-2.0
+
+THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+MERCHANTABLITY OR NON-INFRINGEMENT.
+
+See the Apache Version 2.0 License for specific language governing permissions
+and limitations under the License.
+***************************************************************************** */
+/* global Reflect, Promise */
+
+var extendStatics = function(d, b) {
+	extendStatics =
+		Object.setPrototypeOf ||
+		({ __proto__: [] } instanceof Array &&
+			function(d, b) {
+				d.__proto__ = b;
+			}) ||
+		function(d, b) {
+			for (const p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+		};
+	return extendStatics(d, b);
+};
+
+function __extends(d, b) {
+	extendStatics(d, b);
+	function __() {
+		this.constructor = d;
+	}
+	d.prototype = b === null ? Object.create(b) : ((__.prototype = b.prototype), new __());
+}
+
+var __assign = function() {
+	__assign =
+		Object.assign ||
+		function __assign(t) {
+			for (var s, i = 1, n = arguments.length; i < n; i++) {
+				s = arguments[i];
+				for (const p in s) if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
+			}
+			return t;
+		};
+	return __assign.apply(this, arguments);
+};
+
+function __rest(s, e) {
+	const t = {};
+	for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0) t[p] = s[p];
+	if (s != null && typeof Object.getOwnPropertySymbols === "function")
+		for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++)
+			if (e.indexOf(p[i]) < 0) t[p[i]] = s[p[i]];
+	return t;
+}
+
+function styleInject(css, ref) {
+	if (ref === void 0) ref = {};
+	const {insertAt} = ref;
+
+	if (!css || typeof document === "undefined") {
+		return;
+	}
+
+	const head = document.head || document.getElementsByTagName("head")[0];
+	const style = document.createElement("style");
+	style.type = "text/css";
+
+	if (insertAt === "top") {
+		if (head.firstChild) {
+			head.insertBefore(style, head.firstChild);
+		} else {
+			head.appendChild(style);
+		}
+	} else {
+		head.appendChild(style);
+	}
+
+	if (style.styleSheet) {
+		style.styleSheet.cssText = css;
+	} else {
+		style.appendChild(document.createTextNode(css));
+	}
+}
+
+const css =
+	".magnifier {\n  position: relative;\n  display: inline-block;\n  line-height: 0; }\n\n.magnifier-image {\n  cursor: none; }\n\n.magnifying-glass {\n  position: absolute;\n  z-index: 1;\n  background: #e5e5e5 no-repeat;\n  border: solid #ebebeb;\n  box-shadow: 2px 2px 3px rgba(0, 0, 0, 0.3);\n  opacity: 0;\n  transition: opacity 0.3s;\n  pointer-events: none; }\n  .magnifying-glass.circle {\n    border-radius: 50%; }\n  .magnifying-glass.visible {\n    opacity: 1; }\n";
+styleInject(css);
+
+const Magnifier = (function(_super) {
+	__extends(Magnifier, _super);
+	function Magnifier(props) {
+		const _this = _super.call(this, props) || this;
+		_this.state = {
+			showZoom: false,
+			mgOffsetX: 0,
+			mgOffsetY: 0,
+			relX: 0,
+			relY: 0,
+		};
+		_this.img = null;
+		_this.triggerEl = null;
+		_this.imgBounds = null;
+		_this.onMouseEnter = _this.onMouseEnter.bind(_this);
+		_this.onMouseMove = throttle(_this.onMouseMove.bind(_this), 20, { trailing: false });
+		_this.onMouseOut = _this.onMouseOut.bind(_this);
+		_this.onTouchStart = _this.onTouchStart.bind(_this);
+		_this.onTouchMove = throttle(_this.onTouchMove.bind(_this), 20, { trailing: false });
+		_this.onTouchEnd = _this.onTouchEnd.bind(_this);
+		_this.calcImgBounds = _this.calcImgBounds.bind(_this);
+		_this.calcImgBoundsDebounced = debounce(_this.calcImgBounds, 200);
+		return _this;
+	}
+	Magnifier.prototype.componentDidMount = function() {
+		if (!this.triggerEl) {
+			console.log("USING NEW EVENT TRIGGER: ", this.triggerEl);
+			this.triggerEl = this.img;
+		}
+		this.triggerEl.addEventListener("mouseenter", this.onMouseEnter, { passive: false });
+		this.triggerEl.addEventListener("mousemove", this.onMouseMove, { passive: false });
+		this.triggerEl.addEventListener("mouseout", this.onMouseOut, { passive: false });
+		this.triggerEl.addEventListener("touchstart", this.onTouchStart, { passive: false });
+		this.triggerEl.addEventListener("touchmove", this.onTouchMove, { passive: false });
+		this.triggerEl.addEventListener("touchend", this.onTouchEnd, { passive: false });
+		window.addEventListener("resize", this.calcImgBoundsDebounced);
+		window.addEventListener("scroll", this.calcImgBoundsDebounced, true);
+	};
+	Magnifier.prototype.componentWillUnmount = function() {
+		this.triggerEl.removeEventListener("mouseenter", this.onMouseEnter);
+		this.triggerEl.removeEventListener("mousemove", this.onMouseMove);
+		this.triggerEl.removeEventListener("mouseout", this.onMouseOut);
+		this.triggerEl.removeEventListener("touchstart", this.onTouchStart);
+		this.triggerEl.removeEventListener("touchmove", this.onTouchMove);
+		this.triggerEl.removeEventListener("touchend", this.onTouchEnd);
+		window.removeEventListener("resize", this.calcImgBoundsDebounced);
+		window.removeEventListener("scroll", this.calcImgBoundsDebounced, true);
+	};
+	Magnifier.prototype.onMouseEnter = function() {
+		this.calcImgBounds();
+	};
+	Magnifier.prototype.onMouseMove = function(e) {
+		const _a = this.props;
+			const {mgMouseOffsetX} = _a;
+			const {mgMouseOffsetY} = _a;
+		if (this.imgBounds) {
+			const {target} = e;
+			const relX = (e.clientX - this.imgBounds.left) / target.clientWidth;
+			const relY = (e.clientY - this.imgBounds.top) / target.clientHeight;
+			this.setState({
+				mgOffsetX: mgMouseOffsetX,
+				mgOffsetY: mgMouseOffsetY,
+				relX,
+				relY,
+				showZoom: true,
+			});
+		}
+	};
+	Magnifier.prototype.onMouseOut = function() {
+		this.setState({
+			showZoom: false,
+		});
+	};
+	Magnifier.prototype.onTouchStart = function(e) {
+		e.preventDefault();
+		this.calcImgBounds();
+	};
+	Magnifier.prototype.onTouchMove = function(e) {
+		e.preventDefault();
+		if (this.imgBounds) {
+			const {target} = e;
+			const _a = this.props;
+				const {mgTouchOffsetX} = _a;
+				const {mgTouchOffsetY} = _a;
+			const relX = (e.targetTouches[0].clientX - this.imgBounds.left) / target.clientWidth;
+			const relY = (e.targetTouches[0].clientY - this.imgBounds.top) / target.clientHeight;
+			if (relX >= 0 && relY >= 0 && relX <= 1 && relY <= 1) {
+				this.setState({
+					mgOffsetX: mgTouchOffsetX,
+					mgOffsetY: mgTouchOffsetY,
+					relX,
+					relY,
+					showZoom: true,
+				});
+			} else {
+				this.setState({
+					showZoom: false,
+				});
+			}
+		}
+	};
+	Magnifier.prototype.onTouchEnd = function() {
+		this.setState({
+			showZoom: false,
+		});
+	};
+	Magnifier.prototype.calcImgBounds = function() {
+		if (this.img) {
+			this.imgBounds = this.img.getBoundingClientRect();
+		}
+	};
+	Magnifier.prototype.render = function() {
+		const _this = this;
+		const _a = this.props;
+			const {src} = _a;
+			const {width} = _a;
+			const {height} = _a;
+			const {className} = _a;
+			const {zoomImgSrc} = _a;
+			const {zoomFactor} = _a;
+			const {mgHeight} = _a;
+			const {mgWidth} = _a;
+			const {mgBorderWidth} = _a;
+			const {mgMouseOffsetX} = _a;
+			const {mgMouseOffsetY} = _a;
+			const {mgTouchOffsetX} = _a;
+			const {mgTouchOffsetY} = _a;
+			const {mgShape} = _a;
+			const {mgShowOverflow} = _a;
+			const otherProps = __rest(_a, [
+				"src",
+				"width",
+				"height",
+				"className",
+				"zoomImgSrc",
+				"zoomFactor",
+				"mgHeight",
+				"mgWidth",
+				"mgBorderWidth",
+				"mgMouseOffsetX",
+				"mgMouseOffsetY",
+				"mgTouchOffsetX",
+				"mgTouchOffsetY",
+				"mgShape",
+				"mgShowOverflow",
+			]);
+		const _b = this.state;
+			const {mgOffsetX} = _b;
+			const {mgOffsetY} = _b;
+			const {relX} = _b;
+			const {relY} = _b;
+			const {showZoom} = _b;
+		let mgClasses = "magnifying-glass";
+		if (showZoom) {
+			mgClasses += " visible";
+		}
+		if (mgShape === "circle") {
+			mgClasses += " circle";
+		}
+		return React.createElement(
+			"div",
+			{
+				className: `magnifier ${  className}`,
+				style: {
+					width,
+					height,
+					overflow: mgShowOverflow ? "visible" : "hidden",
+				},
+			},
+			React.createElement(
+				"img",
+				__assign(
+					{ className: "magnifier-image", src, width: "100%", height: "100%" },
+					otherProps,
+					{
+						onLoad() {
+							_this.calcImgBounds();
+						},
+						ref(e) {
+							_this.img = e;
+						},
+					},
+				),
+			),
+			this.imgBounds &&
+				React.createElement("div", {
+					className: mgClasses,
+					style: {
+						width: mgWidth,
+						height: mgHeight,
+						left:
+							`calc(${ 
+							relX * 100 
+							}% - ${ 
+							mgWidth / 2 
+							}px + ${ 
+							mgOffsetX 
+							}px - ${ 
+							mgBorderWidth 
+							}px)`,
+						top:
+							`calc(${ 
+							relY * 100 
+							}% - ${ 
+							mgHeight / 2 
+							}px + ${ 
+							mgOffsetY 
+							}px - ${ 
+							mgBorderWidth 
+							}px)`,
+						backgroundImage: `url(${  zoomImgSrc || src  })`,
+						backgroundPosition:
+							`calc(${ 
+							relX * 100 
+							}% + ${ 
+							mgWidth / 2 
+							}px - ${ 
+							relX * mgWidth 
+							}px) calc(${ 
+							relY * 100 
+							}% + ${ 
+							mgHeight / 2 
+							}px - ${ 
+							relY * mgWidth 
+							}px)`,
+						backgroundSize:
+							`${zoomFactor * this.imgBounds.width  }% ${  zoomFactor * this.imgBounds.height  }%`,
+						borderWidth: mgBorderWidth,
+					},
+				}),
+		);
+	};
+	Magnifier.defaultProps = {
+		width: "100%",
+		height: "auto",
+		className: "",
+		triggerEl: false,
+		zoomFactor: 1.5,
+		mgWidth: 150,
+		mgHeight: 150,
+		mgBorderWidth: 2,
+		mgShape: "circle",
+		mgShowOverflow: true,
+		mgMouseOffsetX: 0,
+		mgMouseOffsetY: 0,
+		mgTouchOffsetX: -50,
+		mgTouchOffsetY: -50,
+	};
+	return Magnifier;
+})(PureComponent);
+
+export default Magnifier;
